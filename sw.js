fc548814c90eb7c952cac4dd7cf7811982834234
JUAN Madrigal <onemadrigal2@gmail.com>
2026-08-25 20:19:16 +0000

    ALZA v0.2

---
+++ sw.js
@@ -0,0 +1,26 @@
+const C = "alza-v0-2-2";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(C).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(ks => Promise.all(ks.filter(k => k !== C).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener("fetch", e => {
+  const req = e.request;
+  if (req.method !== "GET") return;
+  const url = new URL(req.url);
+  if (url.origin !== location.origin) return;
+  e.respondWith(
+    caches.match(req).then(hit =>
+      hit || fetch(req).then(res => {
+        const cp = res.clone();
+        caches.open(C).then(c => c.put(req, cp));
+        return res;
+      }).catch(() => caches.match("./index.html"))
+    )
+  );
+});
